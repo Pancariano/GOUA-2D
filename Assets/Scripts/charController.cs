e7f6ceb2eb7a9f34a628e69d82378b56b498c7e7
Burak Ekemen <bekemen98@gmail.com>
2023-12-28 21:55:50 +0300

    add sprint renderer flip and input manager

--- Assets/Scripts/charController.cs
+++ Assets/Scripts/charController.cs
@@ -4,40 +4,45 @@ using UnityEngine;
 
 public class charController : MonoBehaviour
 {
-    public float speed = 0.0f;
+    public float speed = 1.0f;
     Rigidbody2D _rb2D;
     Animator _animator;
     Vector3 _charPosition;
-    [SerializeField] private GameObject _camera;
-
-    void Start()
-    {
-        _rb2D = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
-        _charPosition = transform.position;
-    }
+    [SerializeField] GameObject _camera;
+    SpriteRenderer _spriteRenderer;
 
     void FixedUpdate()
     {
         //_rb2D.velocity = new Vector2(speed, .0f);
     }
 
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _rb2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _charPosition = transform.position;
+    }
+
     void Update()
     {
-        _charPosition = new Vector2(_charPosition.x + (speed * Time.deltaTime), _charPosition.y);
+        _charPosition = new Vector3(_charPosition.x + (Input.GetAxis("Horizontal") * speed * Time.deltaTime), _charPosition.y);
         transform.position = _charPosition;
-        _animator.SetFloat("speed", speed);
-       
-        if (Input.GetKey(KeyCode.D))
-            speed = 1f;
-        else if (Input.GetKey(KeyCode.A))
-            speed = -1f;
-        else
-            speed = 0f;
+
+        if (Input.GetAxis("Horizontal") == 0f)
+            _animator.SetFloat("speed", 0f);
+        else 
+            _animator.SetFloat("speed", 1.0f);
+
+        if (Input.GetAxis("Horizontal") > 0.1f)
+            _spriteRenderer.flipX = false;     
+        else if (Input.GetAxis("Horizontal") < 0.1f)
+            _spriteRenderer.flipX=true;
+        
     }
 
     void LateUpdate()
     {
-        _camera.transform.position = new Vector3(_charPosition.x, _charPosition.y, (_charPosition.z - 1f));
+       // _camera.transform.position = new Vector3(_charPosition.x, _charPosition.y, (_charPosition.z - 1f));
     }
 }
