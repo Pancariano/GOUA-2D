6361c26c37cf7130adfdc44f199756d14cf74dfb
Burak Ekemen <bekemen98@gmail.com>
2023-12-28 20:43:34 +0300

    add cam follow

--- Assets/Scripts/charController.cs
+++ Assets/Scripts/charController.cs
@@ -7,26 +7,37 @@ public class charController : MonoBehaviour
     public float speed = 0.0f;
     Rigidbody2D _rb2D;
     Animator _animator;
+    Vector3 _charPosition;
+    [SerializeField] private GameObject _camera;
 
     void Start()
     {
         _rb2D = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        _charPosition = transform.position;
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        _rb2D.velocity = new Vector2(speed, .0f);
+        //_rb2D.velocity = new Vector2(speed, .0f);
     }
 
     void Update()
     {
+        _charPosition = new Vector2(_charPosition.x + (speed * Time.deltaTime), _charPosition.y);
+        transform.position = _charPosition;
         _animator.SetFloat("speed", speed);
        
         if (Input.GetKey(KeyCode.D))
-        { speed = 1.0f; }
-
+            speed = 1f;
+        else if (Input.GetKey(KeyCode.A))
+            speed = -1f;
         else
-        { speed = 0.0f; }
+            speed = 0f;
+    }
+
+    void LateUpdate()
+    {
+        _camera.transform.position = new Vector3(_charPosition.x, _charPosition.y, (_charPosition.z - 1f));
     }
 }
